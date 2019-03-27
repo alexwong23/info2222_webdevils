0c4fb6304bccb406cc87a9d5ff256f9d29de3b59
Qiangsheng Gao <gao.qiangsheng.2015@vjc.sg>
2019-03-28 08:24:51 +1100

    create virtual dev env

--- routes/index.py
+++ routes/index.py
@@ -9,18 +9,21 @@ cur = con.cursor()
 
 indexRouter = Bottle()
 
+
 @indexRouter.route('/')
 def homepage():
     info = {
         'addr': request.remote_addr,
         'environ': request.environ['HTTP_USER_AGENT']
     }
-    return template('home.tpl', info) # unsafe from malicious content
+    return template('home.tpl', info)  # unsafe from malicious content
+
 
 @indexRouter.route('/contactus')
 def contactus():
     info = {}
     return template('contactus.tpl', info)
+
 
 @indexRouter.route('/login')
 def login():
@@ -28,18 +31,21 @@ def login():
         'user': {'unikey': ""},
         'message': ''
     }
-    return template('login.tpl', info) # unsafe from malicious content
+    return template('login.tpl', info)  # unsafe from malicious content
+
 
 @indexRouter.route('/login', method="POST")
 def loginhandler():
     unikey = request.forms.get('unikey')
     password = request.forms.get('password')
     errors = helperMethods.validateForm(request.forms, ['unikey', 'password'])
-    cur.execute('SELECT unikey, password, name, is_admin FROM users WHERE unikey=(?)', (unikey,))
+    cur.execute(
+        'SELECT unikey, password, name, is_admin FROM users WHERE unikey=(?)', (unikey,))
     user = helperMethods.userToDict(cur.fetchone())
     if(user is not None and unikey and password):
         if(user['unikey'] == unikey and user['password'] == password):
-            response.set_cookie('unikey', user['unikey'], secret=COOKIE_SECRET_KEY)
+            response.set_cookie(
+                'unikey', user['unikey'], secret=COOKIE_SECRET_KEY)
             redirect('/users/' + unikey)
         else:
             errors.append('Login Failed: Invalid UniKey or Password.')
@@ -51,11 +57,12 @@ def loginhandler():
     }
     return template('login.tpl', info)
 
+
 @indexRouter.route('/logout')
 def logout():
     # if user is NOT logged in
         # redirect back to login page
     # if logged if __name__ == '__main__':
     response.delete_cookie('unikey')
-        # remove user from session?
+    # remove user from session?
     return redirect('/')
