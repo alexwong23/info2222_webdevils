ed38cd6f07151e364a0a72957acfb99a48fc3b0f
QSFTW <gao.qiangsheng.2015@vjc.sg>
2019-04-07 20:55:56 +1000

    Profile page bootstraped and change password validation

--- routes/users.py
+++ routes/users.py
@@ -1,6 +1,7 @@
 from bottle import template, request, redirect
 
-import random, helperMethods
+import random
+import helperMethods
 '''
     Our Model class
     This should control the actual "logic" of your website
@@ -13,15 +14,17 @@ import random, helperMethods
 import sqlite3
 con = sqlite3.connect('./db/webdevils.db')
 cur = con.cursor()
-COOKIE_SECRET_KEY = "some-secret" # prevent cookie manipulation
+COOKIE_SECRET_KEY = "some-secret"  # prevent cookie manipulation
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Users
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
 
 def redirect_profile_page():
     user = helperMethods.token_user_info()
     redirect('/users/' + user['unikey'])
+
 
 def profile_page(unikey):
     user = helperMethods.token_user_info()
@@ -31,16 +34,17 @@ def profile_page(unikey):
             'user': user,
             'any_user': any_user
         })
-    else: # user not logged in
+    else:  # user not logged in
         return template('error.tpl', {
             'user': user,
             'title': 'Error: Unable to access page',
             'error_message': 'You have to login to view this page.'
         })
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Edit Profile
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
 
 def edit_profile_page(unikey):
     user = helperMethods.token_user_info()
@@ -48,29 +52,32 @@ def edit_profile_page(unikey):
         return template('editprofile.tpl', {
             'user': user
         })
-    else: # user not logged in
+    else:  # user not logged in
         return template('error.tpl', {
             'user': user,
             'title': 'Error: Unable to access page',
             'error_message': 'You have to login to view this page.'
         })
+
 
 def edit_profile_check(first_name, last_name):
     user = helperMethods.token_user_info()
     if(user['unikey'] != ""):
-        cur.execute('UPDATE users SET first_name=(?), last_name=(?) WHERE unikey=(?)', (first_name, last_name, user['unikey']))
+        cur.execute('UPDATE users SET first_name=(?), last_name=(?) WHERE unikey=(?)',
+                    (first_name, last_name, user['unikey']))
         con.commit()
         return redirect(f"/users/{user['unikey']}")
-    else: # user not logged in
+    else:  # user not logged in
         return template('error.tpl', {
             'user': user,
             'title': 'Error: Unable to access page',
             'error_message': 'You have to login to view this page.'
         })
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Change Password
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
 
 def change_password_page(unikey):
     user = helperMethods.token_user_info()
@@ -79,7 +86,7 @@ def change_password_page(unikey):
             'user': user,
             'error': ''
         })
-    else: # user not logged in
+    else:  # user not logged in
         return template('error.tpl', {
             'user': user,
             'title': 'Error: Unable to access page',
@@ -90,30 +97,38 @@ def change_password_page(unikey):
 def change_password_check(new_password, confirm_password):
     user = helperMethods.token_user_info()
     if(user['unikey'] != ""):
+        if len(new_password) < 8 or len(confirm_password) < 8:
+            return template('changepassword.tpl', {
+                'user': user,
+                'error': 'Passwords has to be at least 8 characters long. Please re-enter your new password.'
+            })
         if (new_password == confirm_password):
-            cur.execute('UPDATE users SET password=(?) WHERE unikey=(?)', (new_password, user['unikey']))
+            cur.execute('UPDATE users SET password=(?) WHERE unikey=(?)',
+                        (new_password, user['unikey']))
             con.commit()
-            return redirect("/users/{user['unikey']}")
+
+            return redirect(f"/users/{user['unikey']}")
         else:
-            return template('changepassword.tpl', info = {
+            return template('changepassword.tpl', {
                 'user': user,
                 'error': 'Passwords do not match. Please re-enter your new password.'
             })
-    else: # user not logged in
+    else:  # user not logged in
         return template('error.tpl', {
             'user': user,
             'title': 'Error: Unable to access page',
             'error_message': 'You have to login to view this page.'
         })
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Search Messages
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
 
 def search_users(query):
     user = helperMethods.token_user_info()
     if(user['unikey'] != ""):
-        if(query is None or query == ""): # prevents error in front end
+        if(query is None or query == ""):  # prevents error in front end
             return template('search_users', {
                 'user': user,
                 'query': query,
@@ -133,7 +148,7 @@ def search_users(query):
                 'query': query,
                 'results': results
             })
-    else: # user not logged in
+    else:  # user not logged in
         return template('error.tpl', {
             'user': user,
             'title': 'Error: Unable to access page',
--- views/userProfile.tpl
+++ views/userProfile.tpl
@@ -1,11 +1,33 @@
 % rebase('layout.tpl', status=user['status'])
 
-<h4>User's name is {{any_user['first_name']}} {{any_user['last_name']}}</h4>
-<h4>with an id of {{any_user['unikey']}}</h4>
 
-% if(user['unikey'] == any_user['unikey']):
-  <a href="/users/{{user['unikey']}}/edit" class="btn btn-primary active mr-2" role="button" aria-pressed="true">Edit Profile</a>
-  <a href="/users/{{user['unikey']}}/changepassword" class="btn btn-primary active mr-1" role="button" aria-pressed="true">Change Password</a>
-% else:
-  <a href="/messages/{{any_user['unikey']}}" class="btn btn-primary active mr-1" role="button" aria-pressed="true">Message</a>
-% end
+<div class="container-fluid">
+    <div class="row justify-content-md-center">
+        <h3>Profile</h3>
+    </div>
+    <div class="row justify-content-md-center">
+        <div class="col text-right ">
+        <h4 class="text-secondary">Username:</h4>
+        </div>
+        <div class="col">
+        <h4> {{any_user['first_name']}} {{any_user['last_name']}}</h4>
+        </div>
+    </div>
+
+    <div class="row justify-content-md-center">
+        <div class="col text-right">
+        <h4 class="text-secondary">ID:</h4>
+        </div>
+        <div class="col">
+        <h4> {{any_user['unikey']}}</h4>
+        </div>
+    </div>
+    % if(user['unikey'] == any_user['unikey']):
+    <div class="row justify-content-md-center">
+        <a href="/users/{{user['unikey']}}/edit" class="btn btn-primary active mr-2" role="button" aria-pressed="true">Edit Profile</a>
+        <a href="/users/{{user['unikey']}}/changepassword" class="btn btn-primary active mr-1" role="button" aria-pressed="true">Change Password</a>
+        % else:
+        <a href="/messages/{{any_user['unikey']}}" class="btn btn-primary active mr-1" role="button" aria-pressed="true">Message</a>
+    </div>
+    % end
+</div>
